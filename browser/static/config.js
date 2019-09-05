5d7d1d59593789ecd35b03a309b9f89b1153be47
jinjiuxiang <foreverklmn@sina.com>
2019-09-05 18:55:42 +0800

    必

--- browser/static/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  baseUrl:"https://info.chaindigg.com",
-  pathUrl:"https://info.chaindigg.com",
-  // baseUrl:"https://info.blockdigg.com",
-  // pathUrl:"https://info.blockdigg.com",
-  // baseUrl:window.location.protocol+'//'+window.location.host,
-  // pathUrl:window.location.protocol+'//'+window.location.host,
-}
